83524b7925d7c91549249656d3b30ac2d0b94113
David Salcedo <dvsalcedov@gmail.com>
2020-12-14 15:04:11 -0500

    Changes in the structures

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
 namespace RemoveDuplicates1
 {
     class Program
@@ -13,6 +12,7 @@ namespace RemoveDuplicates1
             list.AddLast(3);
             list.AddLast(1);
             list.AddLast(5);
+            list.AddLast(7);
             list.AddLast(3);
 
             foreach (var item in list)
@@ -30,18 +30,32 @@ namespace RemoveDuplicates1
         public static LinkedList<int> RemoveDuplicate (LinkedList<int> list){
             Hashtable buffer = new Hashtable();
             LinkedList<int> listResult = new LinkedList<int>();
-            if(list.Count <= 1)
+            if(list.Count <= 1){
               return list;
-            
-            foreach (var item in list)
-            {
-              if ( !buffer.ContainsKey(item) ){
-                buffer.Add(item, 1);
-                listResult.AddLast(item);
-              }
             }
 
-            return listResult;
+            LinkedListNode<int> node = list.First;
+            LinkedListNode<int> nodePrevious = null;
+            LinkedListNode<int> nodeNext = null;
+
+
+            while ( node != null ) {
+              if ( !buffer.ContainsKey(node.Value) ){
+                buffer.Add(node.Value, 1);
+              }else {
+                nodePrevious = node.Previous;
+                nodeNext =  node.Next;
+                if (nodeNext != null) {
+                  list.Remove(node.Next);
+                  list.AddAfter(nodePrevious, nodeNext);
+                }
+                list.Remove(node);
+                node = nodePrevious;
+              }
+             
+              node = node.Next;
+            }
+            return list;
         }
     }
 }
